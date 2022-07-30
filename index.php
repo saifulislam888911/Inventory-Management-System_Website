50e6632bf99ac51d23c28fb7ea3b727ef2e8f4e2
saifulislam888911 <saiful.islam888911@mail.com>
2022-07-30 22:43:53 +0600

    index_Commit No. 1.1 _Total Main Commit No. 1

--- index.php
+++ index.php
@@ -1,12 +1,59 @@
+<?php
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Inventory Management System</title>
+
+
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
+    <!-- MY CSS -->
+    <link rel="stylesheet" href="styles/style.css">
+
 </head>
+
+
+
+
+
 <body>
-    
+    <div>
+        <p class="center_align">
+        Welcome To Big-Bazar Inventory System
+        </p>
+    </div>
+
+    <div class="col-sm-6">
+        <a href="http://login.php" target="_blank" rel="noopener noreferrer">
+            <input type="button" class="btn btn-danger pull-right" value="Log In">
+        </a>
+    </div>
+
+    <div class="col-sm-6">
+        <a href="http://register.php" target="_blank" rel="noopener noreferrer">
+            <input type="button" class="btn btn-success pull-left" value="Register">
+        </a>
+    </div>
 </body>
 </html>
